5332ef0a1f252266b953cad4c597bbdafcb7809f
Aleksei Alekseev <blackcloudishere@gmail.com>
2025-02-19 18:28:12 +0300

    Added solution to 0301

--- 03 - Military/0301 - Shooting Range 0.py
+++ 03 - Military/0301 - Shooting Range 0.py
@@ -1,1 +1,16 @@
-pass
+from pyjop import *
+
+SimEnv.connect()
+env = SimEnvManager.first()
+env.reset()
+rifle = SniperRifle.first()
+
+while SimEnv.run_main():
+    dects = rifle.get_object_detections()
+    for target_id in dects:
+        if target_id.entity_name.startswith("TargetRed"):
+            rifle.fire()
+        else:
+            continue
+
+SimEnv.disconnect()
